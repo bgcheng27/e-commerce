d32c70821697d368bea3c8220de112b3a7ba0b96
Brian Cheng <bgcheng27@gmail.com>
2023-09-21 12:10:24 -0700

    Began working on UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,34 @@
+import { useEffect, useState } from "react"
+import "../public/styles.css"
+
 function App() {
+  const [products, setProducts] = useState([]);
+
   return (
     <>
-      <h1>Troll</h1>
+      <div className="navbar">
+        <div className="nav-left">
+          
+        </div>
+        <div className="nav-mid">
+          <form className="search-form">
+            <div className="input-group">
+              <button>Dropdown</button>
+              <input name="searchInput" className="search" type="text"></input>
+              <button type="submit">Submit</button>
+            </div>
+          </form>
+        </div>
+        <button className="sell-btn">Sell</button>
+        <div className="nav-right">
+          <div className="pfp">
+
+          </div>
+        </div>
+      </div>
+      <div>
+        <h1>Body</h1>
+      </div>
     </>
   )
 }
